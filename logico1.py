5a6db4b520d557b5f714dc84bccbd6ef1e65bf23
EndmostDock407 <jon117pecho@hotmail.com>
2023-12-01 22:47:22 -0600

    Add files via upload

---
+++ logico1.py
@@ -0,0 +1,12 @@
+
+# operador logico usando condicional if, else
+print ("inserta valor a ");
+a = input();
+print ("inserta valor b");
+b = input();
+# test expression
+if a < b:
+    # statement to be run
+    print("b " + b + " es mayor que " + "a " +a);
+else:
+    print ("b " + b + "  es menor que " + "a " +a);
---
+++ logico2.py
@@ -0,0 +1,10 @@
+
+# operador logico usando condicional if,elsif,else
+a = 27
+b = 93
+if a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+else: 
+    print ("a is equal to b")
